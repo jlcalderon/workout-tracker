9890d1ab9415e49a0fc27752d095bf6c5cb26201
JLC <jlc@annonymous.com>
2020-11-12 15:16:29 -0600

    Deploy in heroku with MongoDB Atlas

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ require("./routes/htmlRoutes")(app);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 });
 
